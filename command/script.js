569a630407ea17ab301c7d7c9cce4c98213e01cc
rongzhenxin <zx.rong@zuolin.com>
2019-03-28 15:13:03 +0800

    过滤类型为文件的数据

--- command/script.js
+++ command/script.js
@@ -1,18 +1,27 @@
 var fs = require('fs');
 var path = require('path');
 var inquirer = require('inquirer');
-
-fs.readdir(path.resolve(__dirname, './functions'), function(err, folders){
+var basePath = __dirname + '/functions';
+fs.readdir(basePath, function(err, folders){
   if (err) {
     throw err;
   }
+  var dirs = [];
+  folders.map(function(file){
+    var stat = fs.statSync(basePath + '/' + file);
+    console.log(file, stat.isDirectory())
+    if (stat.isDirectory()) {
+      dirs.push(file);
+    }
+  });
+  
   inquirer.prompt({
     type: 'input',
     name: 'name',
     message: '请输入模块名，模块名必须短线小写word连接，示例 hello-world：',
     validate: function(answer) {
       var moduleName = answer;
-      var moduleExist = folders.filter(item => item === moduleName).length ? true : false;
+      var moduleExist = dirs.filter(item => item === moduleName).length ? true : false;
       if (moduleExist) {
         return '模块已经存在';
       }
